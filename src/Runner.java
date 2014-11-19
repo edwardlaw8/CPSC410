1c77db1f9b5f97b4c5b3427dfedc659a64395e84
unknown <jackyliao4@gmail.com>
2014-11-18 23:31:09 -0800

    mod runner input

--- src/Runner.java
+++ src/Runner.java
@@ -14,17 +14,24 @@ public class Runner {
 		String pmd_loc = dir + "/pmd/bin/pmd";
 		String pattern = "violation";
 		String src = dir + "/Sponge(CPSC410CodeBase1)";
+		String src3 = dir + "/spark-master(CPSC410CodeBase3)";
 		String src2 = dir + "/twoway-view(CPSC410CodeBase2)";
        
-        System.out.println("user.dir is : " + dir);
+        //System.out.println("user.dir is : " + dir);
 		Runtime rt = Runtime.getRuntime();
 		Scanner s = new Scanner(System.in);
-		String userinput;
+		String userinputStr;
 	
-		System.out.println("Which Codebase Would You Like to Analyze? Sponge or Twoway-view?");
-		userinput = s.next();
-		if (userinput.equalsIgnoreCase("Sponge")) {
-			PMDAnalyzer pmdAnalyzer = new PMDAnalyzer(pmd_loc, pattern, src);
+		System.out.println("Which Codebase Would You Like to Analyze? \nEnter (1) for Sponge \nEnter (2) Twoway-view?");
+		int userinput = -1;
+		do{
+			System.out.print("Please enter a valid response: ");
+			userinputStr = s.next();
+			userinput = Integer.parseInt(userinputStr);
+			System.out.println("You entered: " + userinput );
+		}while(userinput!= 1 && userinput!= 2 );
+		if (userinput == 1) {
+			PMDAnalyzer pmdAnalyzer = new PMDAnalyzer(pmd_loc, pattern, src3);
 			int violations = pmdAnalyzer.analyzeThis();
 			
 			JDAnalyzer jdAnalyzer = new JDAnalyzer(src);
@@ -32,7 +39,7 @@ public class Runner {
 			Fuser fuser = new Fuser(violations, packageList);
 		}
 		
-		if (userinput.equalsIgnoreCase("Twoway-view")) {
+		if (userinput == 2) {
 			PMDAnalyzer pmdAnalyzer = new PMDAnalyzer(pmd_loc, pattern, src2);
 			int violations = pmdAnalyzer.analyzeThis();
 		
